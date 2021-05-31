768f3d010f97d03b1bdf1b9e3ebdc3d55b6c8c8f
Vui-Chee <vuicheesiew@gmail.com>
2021-05-31 11:36:56 +0800

    Remove unexpected license

--- Casks/tetris.rb
+++ Casks/tetris.rb
@@ -4,7 +4,6 @@ cask "tetris" do
   homepage "https://github.com/Vui-Chee/Tetris-Solarized"
   url "https://github.com/Vui-Chee/Tetris-Solarized/releases/download/v1.0/tetris-solarized-darwin-x64.zip"
   sha256 "9854cc35236f67ccbfe8cf12843e9b5ab302810e6783a1cba7849a52e576301f"
-  license "MIT"
   
   app "tetris-solarized-darwin-x64/tetris-solarized.app"
 end
