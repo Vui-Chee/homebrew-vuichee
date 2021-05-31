cdbbb07e371c6c23241a8052f1d6356e54412b52
Vui-Chee <vuicheesiew@gmail.com>
2021-05-31 11:41:41 +0800

    Fixes according to homebrew audit/style
    Using `brew style --fix` and `brew audit --cask`.

--- Casks/tetris.rb
+++ Casks/tetris.rb
@@ -1,9 +1,12 @@
 cask "tetris" do
+  version "1.0"
+  sha256 "9854cc35236f67ccbfe8cf12843e9b5ab302810e6783a1cba7849a52e576301f"
+
+  url "https://github.com/Vui-Chee/Tetris-Solarized/releases/download/v1.0/tetris-solarized-darwin-x64.zip"
   name "Tetris"
   desc "Desktop game of tetris"
   homepage "https://github.com/Vui-Chee/Tetris-Solarized"
-  url "https://github.com/Vui-Chee/Tetris-Solarized/releases/download/v1.0/tetris-solarized-darwin-x64.zip"
-  sha256 "9854cc35236f67ccbfe8cf12843e9b5ab302810e6783a1cba7849a52e576301f"
-  
+
   app "tetris-solarized-darwin-x64/tetris-solarized.app"
 end
+
