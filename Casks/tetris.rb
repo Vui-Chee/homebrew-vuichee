de5d46f64bb18e191236459fb9c141830cd74222
Vui-Chee <vuicheesiew@gmail.com>
2021-05-31 11:45:40 +0800

    Provide url versioning + remove trailing newline

--- Casks/tetris.rb
+++ Casks/tetris.rb
@@ -2,11 +2,10 @@ cask "tetris" do
   version "1.0"
   sha256 "9854cc35236f67ccbfe8cf12843e9b5ab302810e6783a1cba7849a52e576301f"
 
-  url "https://github.com/Vui-Chee/Tetris-Solarized/releases/download/v1.0/tetris-solarized-darwin-x64.zip"
+  url "https://github.com/Vui-Chee/Tetris-Solarized/releases/download/v#{version}/tetris-solarized-darwin-x64.zip"
   name "Tetris"
   desc "Desktop game of tetris"
   homepage "https://github.com/Vui-Chee/Tetris-Solarized"
 
   app "tetris-solarized-darwin-x64/tetris-solarized.app"
 end
-
